e73eba7af386bf86acc2598c86a4eef1cb3a6bc0
CHEN YONGJIN <17010340@rp.edu.sg>
2018-07-17 12:01:14 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/custommovielist/CustomAdapter.java
@@ -0,0 +1,15 @@
+package sg.edu.rp.c346.custommovielist;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    //TODO
+    public CustomAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+    }
+}
